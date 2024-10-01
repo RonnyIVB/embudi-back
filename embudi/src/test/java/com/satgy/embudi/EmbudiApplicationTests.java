7781bdc78224c914a354d598a4f61ecdc05a9757
Ronny <rocknny@hotmail.com>
2024-10-01 16:42:30 -0500

    First commit

---
+++ embudi/src/test/java/com/satgy/embudi/EmbudiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satgy.embudi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmbudiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
